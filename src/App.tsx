d89a10fcea09007cfa2a4a9ba52ec9d9a08601d4
xloc <xloc.cc@outlook.com>
2022-08-12 16:17:00 -0700

    fit plot into range

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import classNames from 'classnames';
 import { useEffect, useRef, useState } from 'react';
 import _ from "lodash";
 import { useWebSocket } from './useWebSocket';
+import wave from './wave.json';
 
 
 class Oscilloscope {
   records: Record[];
   drawRequestID?: number;
-  tRange: number = 20;
-  vRange: number = 2;
+  tRange: number = 10;
+  tEnd: number = 11;
+  vRange: number = 4;
   vCenter: number = 0;
 
 
@@ -31,11 +33,7 @@ class Oscilloscope {
 
     const drawLoop = () => {
       ctx.clearRect(0, 0, width, height);
-      ctx.beginPath();
-      ctx.lineTo(0, 0);
-      ctx.lineTo(width / 2, height / 3 * 2);
-      ctx.lineTo(width, height);
-      ctx.stroke();
+      this.draw(ctx, 0, 0, width, height);
     };
 
     this.drawRequestID = requestAnimationFrame(drawLoop);
@@ -45,9 +43,46 @@ class Oscilloscope {
     this.drawRequestID && cancelAnimationFrame(this.drawRequestID);
   }
 
-  draw(ctx: CanvasRenderingContext2D, x: number, y: number, h: number, w: number) {
+  draw(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number) {
+    ctx.beginPath();
+    // ctx.lineTo(0, 0);
+    // ctx.lineTo(w / 2, h / 3);
+    // ctx.lineTo(w, h);
 
+    const getT: any = (o: { t: number }) => o.t;
+
+    let i = wave.length - 1;
+    let tStart = getT(wave[i]) - this.tRange;
+    if (this.tEnd !== Infinity) {
+      i = bisect_left(wave, getT, this.tEnd);
+      tStart = getT(wave[i]) - this.tRange;
+    }
+    for (; i >= 0 && tStart < getT(wave[i]); i--) {
+      const pt = wave[i];
+      const value = pt.bb;
+      const t = pt.t;
+
+      const x = w - t * w / this.tRange;
+      const y = h - value * h / this.vRange - h / 2;
+
+      ctx.lineTo(x, y);
+    }
+
+    ctx.stroke();
   }
+}
+
+function bisect_left<T>(arr: T[], key: (arg: T) => number, t: number): number {
+  let lo = 0, hi = arr.length - 1;
+  while (lo < hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (t < key(arr[mid])) {
+      hi = mid;
+    } else {
+      lo = mid + 1
+    }
+  }
+  return lo;
 }
 
 
@@ -81,6 +116,14 @@ function App() {
     };
   }, [canvasRef]);
 
+
+  useEffect(() => {
+    (wave as { aa: number }[])
+      .map(({ aa }) => {
+        return aa;
+      });
+
+  }, [])
 
 
 
